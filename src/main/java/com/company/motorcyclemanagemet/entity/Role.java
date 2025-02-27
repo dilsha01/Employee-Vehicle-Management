52da02b4d80caab78ac9af31166accace65422fa
dilsha01 <mihirangangd.21@uom.lk>
2025-02-27 11:28:21 +0530

    Updated Role Entity

--- src/main/java/com/company/motorcyclemanagemet/entity/Role.java
+++ src/main/java/com/company/motorcyclemanagemet/entity/Role.java
@@ -1,5 +1,10 @@
 package com.company.motorcyclemanagemet.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class Role {
     @Id
