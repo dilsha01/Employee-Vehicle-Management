a070489ecda814706dcb44424a4e4d683a87ba4e
dilsha01 <mihirangangd.21@uom.lk>
2025-03-01 18:24:21 +0530

    Updated Accident Entity

---
+++ src/main/java/com/company/motorcyclemanagement/entity/Accident.java
@@ -0,0 +1,31 @@
+package com.company.motorcyclemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Accident {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accidentId;
+
+    @ManyToOne
+    @JoinColumn(name = "motorcycleId", nullable = false)
+    private MotorCycle motorcycle;
+
+    private LocalDateTime accidentDate;
+    private String description;
+
+    @Lob
+    private byte[] documents;
+
+    @ManyToOne
+    @JoinColumn(name = "notedBy", nullable = false)
+    private OtherEmployee notedBy;
+
+    private LocalDateTime notedDate;
+}
