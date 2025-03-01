f8556cfc574db45656aa9897950e52dce11e9611
dilsha01 <mihirangangd.21@uom.lk>
2025-03-01 18:25:44 +0530

    Updated Maintenance Entity

---
+++ src/main/java/com/company/motorcyclemanagement/entity/Maintenance.java
@@ -0,0 +1,31 @@
+package com.company.motorcyclemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Maintenance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long maintenanceId;
+
+    @ManyToOne
+    @JoinColumn(name = "motorcycleId", nullable = false)
+    private MotorCycle motorcycle;
+
+    private LocalDateTime maintenanceDate;
+    private Double amount;
+
+    @Lob
+    private byte[] documents; // To store PDF/JPEG files as BLOB
+
+    @ManyToOne
+    @JoinColumn(name = "approvedBy", nullable = false)
+    private OtherEmployee approvedBy;
+
+    private LocalDateTime approvedDate;
+}
