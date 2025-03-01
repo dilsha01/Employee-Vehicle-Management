e0ef0f1f80e52b911a81fa8a1e98e03bf102d583
dilsha01 <mihirangangd.21@uom.lk>
2025-03-01 18:27:08 +0530

    Updated Project Files

--- src/main/java/com/company/motorcyclemanagement/MotorcyclemanagemetApplication.java
+++ src/main/java/com/company/motorcyclemanagement/MotorcyclemanagemetApplication.java
@@ -1,4 +1,4 @@
-package com.company.motorcyclemanagemet;
+package com.company.motorcyclemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
