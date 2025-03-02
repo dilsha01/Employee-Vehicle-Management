412296cb5434c9acea952e6a86a68f61af9d276c
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 22:37:34 +0530

    Created RoleDTO

---
+++ src/main/java/com/company/motorcyclemanagement/dto/RoleDTO.java
@@ -0,0 +1,23 @@
+package com.company.motorcyclemanagement.dto;
+
+public class RoleDTO {
+    private Long roleId;
+    private String userRole;
+
+    // Getters and Setters
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
