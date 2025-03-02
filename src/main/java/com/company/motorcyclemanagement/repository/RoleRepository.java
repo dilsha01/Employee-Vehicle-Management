36556ced6a54e03884608296f00c032efb45773a
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 22:38:11 +0530

    Created RoleRepository

---
+++ src/main/java/com/company/motorcyclemanagement/repository/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.company.motorcyclemanagement.repository;
+
+import com.company.motorcyclemanagement.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {}
