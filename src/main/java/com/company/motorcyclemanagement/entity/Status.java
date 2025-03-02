36d8ddd31e1201e3198153bb412cf1a6b4406b63
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 16:02:19 +0530

    Updated Getters Setters in Status

--- src/main/java/com/company/motorcyclemanagement/entity/Status.java
+++ src/main/java/com/company/motorcyclemanagement/entity/Status.java
@@ -36,4 +36,84 @@ public class Status {
     @OneToMany(mappedBy = "status")
     private Set<RunningChart> runningCharts;
 
+    // Getter and Setter for statusId
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    // Getter and Setter for status
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    // Getter and Setter for accidents
+    public Set<Accident> getAccidents() {
+        return accidents;
+    }
+
+    public void setAccidents(Set<Accident> accidents) {
+        this.accidents = accidents;
+    }
+
+    // Getter and Setter for insurances
+    public Set<Insurance> getInsurances() {
+        return insurances;
+    }
+
+    public void setInsurances(Set<Insurance> insurances) {
+        this.insurances = insurances;
+    }
+
+    // Getter and Setter for licences
+    public Set<License> getLicences() {
+        return licences;
+    }
+
+    public void setLicences(Set<License> licences) {
+        this.licences = licences;
+    }
+
+    // Getter and Setter for maintenances
+    public Set<Maintenance> getMaintenances() {
+        return maintenances;
+    }
+
+    public void setMaintenances(Set<Maintenance> maintenances) {
+        this.maintenances = maintenances;
+    }
+
+    // Getter and Setter for motorcycles
+    public Set<MotorCycle> getMotorcycles() {
+        return motorcycles;
+    }
+
+    public void setMotorcycles(Set<MotorCycle> motorcycles) {
+        this.motorcycles = motorcycles;
+    }
+
+    // Getter and Setter for motorcycleAllowances
+    public Set<MotorcycleAllowance> getMotorcycleAllowances() {
+        return motorcycleAllowances;
+    }
+
+    public void setMotorcycleAllowances(Set<MotorcycleAllowance> motorcycleAllowances) {
+        this.motorcycleAllowances = motorcycleAllowances;
+    }
+
+    // Getter and Setter for runningCharts
+    public Set<RunningChart> getRunningCharts() {
+        return runningCharts;
+    }
+
+    public void setRunningCharts(Set<RunningChart> runningCharts) {
+        this.runningCharts = runningCharts;
+    }
 }
