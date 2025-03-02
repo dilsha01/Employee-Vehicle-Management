4d41ee24bac49e081286ecd2d739d412840cabd6
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 17:14:31 +0530

    Created MotorcycleAllowanceRepository

---
+++ src/main/java/com/company/motorcyclemanagement/repository/MotorcycleAllowanceRepository.java
@@ -0,0 +1,7 @@
+package com.company.motorcyclemanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.company.motorcyclemanagement.entity.MotorcycleAllowance;
+
+public interface MotorcycleAllowanceRepository extends JpaRepository<MotorcycleAllowance, Long> {
+}
