54d9a010c5aaef802fce9d9bbed3b825c6bbabd9
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 16:01:07 +0530

    Updated Getters Setters in MotorCycle

--- src/main/java/com/company/motorcyclemanagement/entity/MotorCycle.java
+++ src/main/java/com/company/motorcyclemanagement/entity/MotorCycle.java
@@ -50,6 +50,118 @@ public class MotorCycle {
     @OneToMany(mappedBy = "motorcycle", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<RunningChart> runningCharts;
 
+    // Getters
+    public Long getMotorcycleId() {
+        return motorcycleId;
+    }
 
+    public BikeEmployee getEmployee() {
+        return employee;
+    }
+
+    public String getRegistrationNo() {
+        return registrationNo;
+    }
+
+    public Double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public String getLoanCompletionDate() {
+        return loanCompletionDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public OtherEmployee getEnteredBy() {
+        return enteredBy;
+    }
+
+    public LocalDateTime getEnteredDate() {
+        return enteredDate;
+    }
+
+    public Set<Accident> getAccidents() {
+        return accidents;
+    }
+
+    public Set<Maintenance> getMaintenances() {
+        return maintenances;
+    }
+
+    public Set<Insurance> getInsurances() {
+        return insurances;
+    }
+
+    public Set<License> getLicenses() {
+        return licenses;
+    }
+
+    public Set<MotorcycleAllowance> getMotorcycleAllowances() {
+        return motorcycleAllowances;
+    }
+
+    public Set<RunningChart> getRunningCharts() {
+        return runningCharts;
+    }
+
+    // Setters
+    public void setMotorcycleId(Long motorcycleId) {
+        this.motorcycleId = motorcycleId;
+    }
+
+    public void setEmployee(BikeEmployee employee) {
+        this.employee = employee;
+    }
+
+    public void setRegistrationNo(String registrationNo) {
+        this.registrationNo = registrationNo;
+    }
+
+    public void setLoanAmount(Double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public void setLoanCompletionDate(String loanCompletionDate) {
+        this.loanCompletionDate = loanCompletionDate;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setEnteredBy(OtherEmployee enteredBy) {
+        this.enteredBy = enteredBy;
+    }
+
+    public void setEnteredDate(LocalDateTime enteredDate) {
+        this.enteredDate = enteredDate;
+    }
+
+    public void setAccidents(Set<Accident> accidents) {
+        this.accidents = accidents;
+    }
+
+    public void setMaintenances(Set<Maintenance> maintenances) {
+        this.maintenances = maintenances;
+    }
+
+    public void setInsurances(Set<Insurance> insurances) {
+        this.insurances = insurances;
+    }
+
+    public void setLicenses(Set<License> licenses) {
+        this.licenses = licenses;
+    }
+
+    public void setMotorcycleAllowances(Set<MotorcycleAllowance> motorcycleAllowances) {
+        this.motorcycleAllowances = motorcycleAllowances;
+    }
+
+    public void setRunningCharts(Set<RunningChart> runningCharts) {
+        this.runningCharts = runningCharts;
+    }
 
 }
