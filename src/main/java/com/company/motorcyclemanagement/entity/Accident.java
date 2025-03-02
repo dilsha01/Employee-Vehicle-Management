202984cc04c3dbbe006fbbb8c2018e5bb263dd4a
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 15:59:53 +0530

    Updated Getters Setters in Accident

--- src/main/java/com/company/motorcyclemanagement/entity/Accident.java
+++ src/main/java/com/company/motorcyclemanagement/entity/Accident.java
@@ -33,5 +33,69 @@ public class Accident {
 
     private LocalDateTime notedDate;
 
+    // Getters
+    public Long getAccidentId() {
+        return accidentId;
+    }
 
+    public MotorCycle getMotorcycle() {
+        return motorcycle;
+    }
+
+    public LocalDateTime getAccidentDate() {
+        return accidentDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public byte[] getDocuments() {
+        return documents;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public OtherEmployee getNotedBy() {
+        return notedBy;
+    }
+
+    public LocalDateTime getNotedDate() {
+        return notedDate;
+    }
+
+    // Setters
+    public void setAccidentId(Long accidentId) {
+        this.accidentId = accidentId;
+    }
+
+    public void setMotorcycle(MotorCycle motorcycle) {
+        this.motorcycle = motorcycle;
+    }
+
+    public void setAccidentDate(LocalDateTime accidentDate) {
+        this.accidentDate = accidentDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDocuments(byte[] documents) {
+        this.documents = documents;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setNotedBy(OtherEmployee notedBy) {
+        this.notedBy = notedBy;
+    }
+
+    public void setNotedDate(LocalDateTime notedDate) {
+        this.notedDate = notedDate;
+    }
 }
