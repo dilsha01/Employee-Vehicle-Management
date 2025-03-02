511c00144e44493fb2fea6fd5149d5e150c158e0
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 16:03:24 +0530

    Updated Dependencies

--- src/main/java/com/company/motorcyclemanagement/MotorcyclemanagementApplication.java
+++ src/main/java/com/company/motorcyclemanagement/MotorcyclemanagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MotorcyclemanagemetApplication {
+public class MotorcyclemanagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MotorcyclemanagemetApplication.class, args);
+        SpringApplication.run(MotorcyclemanagementApplication.class, args);
     }
 
 }
