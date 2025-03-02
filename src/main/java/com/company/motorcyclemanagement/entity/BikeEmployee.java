174e3a2c44cd93b38088e48826d48947df1232c2
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 16:00:09 +0530

    Updated Getters Setters in BikeEmployee

--- src/main/java/com/company/motorcyclemanagement/entity/BikeEmployee.java
+++ src/main/java/com/company/motorcyclemanagement/entity/BikeEmployee.java
@@ -26,4 +26,63 @@ public class BikeEmployee {
     private OtherEmployee enteredBy;
 
     private LocalDateTime enteredDate;
+
+
+    // Getters
+    public Long getBikeEmployeeId() {
+        return bikeEmployeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public OtherEmployee getEnteredBy() {
+        return enteredBy;
+    }
+
+    public LocalDateTime getEnteredDate() {
+        return enteredDate;
+    }
+
+    // Setters
+    public void setBikeEmployeeId(Long bikeEmployeeId) {
+        this.bikeEmployeeId = bikeEmployeeId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setEnteredBy(OtherEmployee enteredBy) {
+        this.enteredBy = enteredBy;
+    }
+
+    public void setEnteredDate(LocalDateTime enteredDate) {
+        this.enteredDate = enteredDate;
+    }
 }
