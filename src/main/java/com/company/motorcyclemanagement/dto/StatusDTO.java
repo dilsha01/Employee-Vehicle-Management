a60ad401d0fbbd60acc6e9663ed6f30baea6ebb9
dilsha01 <mihirangangd.21@uom.lk>
2025-03-02 22:38:47 +0530

    Created StatusController

---
+++ src/main/java/com/company/motorcyclemanagement/dto/StatusDTO.java
@@ -0,0 +1,23 @@
+package com.company.motorcyclemanagement.dto;
+
+public class StatusDTO {
+    private Long statusId;
+    private String status;
+
+    // Getters and Setters
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
