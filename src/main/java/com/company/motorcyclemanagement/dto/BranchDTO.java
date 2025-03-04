506c41d335a2d0962f853976e35c98420b77be56
dilsha01 <mihirangangd.21@uom.lk>
2025-03-04 14:39:06 +0530

    Created BranchDTO

---
+++ src/main/java/com/company/motorcyclemanagement/dto/BranchDTO.java
@@ -0,0 +1,23 @@
+package com.company.motorcyclemanagement.dto;
+
+public class BranchDTO {
+    private Long branchId;
+    private String branchName;
+
+    // Getters and Setters
+    public Long getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(Long branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+}
