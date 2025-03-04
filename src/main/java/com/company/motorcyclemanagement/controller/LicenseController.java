42e742bc988e6b7c613e99f39fb3ec6905b95db0
dilsha01 <mihirangangd.21@uom.lk>
2025-03-04 14:49:52 +0530

    Updated APIs

--- src/main/java/com/company/motorcyclemanagement/controller/LicenseController.java
+++ src/main/java/com/company/motorcyclemanagement/controller/LicenseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/license")
+@RequestMapping("api/license")
 public class LicenseController {
     @Autowired
     private LicenseService licenseService;
--- src/main/java/com/company/motorcyclemanagement/controller/RunningChartController.java
+++ src/main/java/com/company/motorcyclemanagement/controller/RunningChartController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/running-chart")
+@RequestMapping("api/running-chart")
 public class RunningChartController {
     @Autowired
     private RunningChartService runningChartService;
