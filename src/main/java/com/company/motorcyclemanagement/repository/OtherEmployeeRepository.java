4a981d80383c9a5d2945b6a0cb7c7815fe670764
dilsha01 <mihirangangd.21@uom.lk>
2025-03-06 22:55:42 +0530

    Updated OtherEmployeeRepository

--- src/main/java/com/company/motorcyclemanagement/repository/OtherEmployeeRepository.java
+++ src/main/java/com/company/motorcyclemanagement/repository/OtherEmployeeRepository.java
@@ -3,5 +3,8 @@ package com.company.motorcyclemanagement.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.company.motorcyclemanagement.entity.OtherEmployee;
 
+import java.util.Optional;
+
 public interface OtherEmployeeRepository extends JpaRepository<OtherEmployee, Long> {
+        Optional<OtherEmployee> findByName(String name);
 }
