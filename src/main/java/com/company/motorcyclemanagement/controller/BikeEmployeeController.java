1930f579191cac9366a75b906169a0954ad3d400
dilsha01 <mihirangangd.21@uom.lk>
2025-03-12 23:20:56 +0530

    Updated BikeEmployeeController

--- src/main/java/com/company/motorcyclemanagement/controller/BikeEmployeeController.java
+++ src/main/java/com/company/motorcyclemanagement/controller/BikeEmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bike-employee")
+@RequestMapping("/api/bike-employee")
 public class BikeEmployeeController {
     @Autowired
     private BikeEmployeeService bikeEmployeeService;
